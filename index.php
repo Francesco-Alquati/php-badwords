c4c9335e3ca3f78f2748eb4010b53ef2bc97f160
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-09-05 17:21:42 +0200

    [UPD] Created first file with form

--- index.php
+++ index.php
@@ -3,9 +3,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Badwords</title>
 </head>
 <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center">PHP Badwords</h1>
+            </div>
+            <div class="col-12">
+                <form action="./badwords.php" method="GET">
+                    <div class="row gy-3">
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label for="" class="control-label">Pargrafo</label>
+                                <textarea name="paragraph" id="paragraph" class="control-label" placeholder="parafrafo"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label for="" class="control-label">Parola da censurare</label>
+                                <input name="word" id="word" class="control-label" placeholder="parola da censurare"></input>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-small btn-primary"></button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
     
 </body>
 </html>
